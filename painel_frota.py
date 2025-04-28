271b7668c4bef56d9ee4e42ca65c0d0681bed9b2
jaquespaes <jppaes@neergo.com.br>
2025-04-28 00:53:44 -0300

    Add files via upload

---
+++ painel_frota.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+st.title('Corelog FleetBrain')
+
+st.write("Este é o painel de gestão de frota da Corelog. Utilize as funcionalidades abaixo para visualizar dados de frota e pedidos.")
